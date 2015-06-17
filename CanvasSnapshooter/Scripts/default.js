6b433f2ac5c5ab2239e47443ec477476fe171822
ericshen881027 <eric.shen881027@hotmail.com>
2015-06-17 21:37:35 +0800

    Learning the canvas

---
+++ CanvasSnapshooter/Scripts/default.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery-2.1.4.js" />
+
+$(document).ready(function () {
+    $('#btnSnapshot').on('click', drawVideoFrame);
+});
+
+function drawVideoFrame() {
+    var canvas = document.getElementById('myCanvas');
+    var video = document.getElementById('myVideo');
+    canvas.getContext('2d').drawImage(video, 0, 0, 360, 240);
+}
